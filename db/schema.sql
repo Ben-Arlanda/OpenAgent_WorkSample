eab2d3c044b776d59a5de6f40f4cda69a6937514
Ben-Arlanda <ben.arlanda@protonmail.com>
2024-08-19 17:58:32 +1000

    added db and dependancies in server, created api contact end point

---
+++ db/schema.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE contact_form;
+
+CREATE TABLE contacts (
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(255),
+    last_name VARCHAR(255),
+    email VARCHAR(255),
+    phone_number VARCHAR(20),
+    message TEXT
+);
